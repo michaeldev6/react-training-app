8ada090dbfe1c7a1212914b255651ebf1490b08f
Michael_Vano <michaelv@dev6.com>
2020-02-21 16:17:44 -0500

    step 05 - added state to toggle notification

--- src/App.js
+++ src/App.js
@@ -6,25 +6,48 @@ import {Notification, notificationStyles} from "./components/core/Notification";
 // Just sample component to display how it can be passed in through props
 function ExampleNotificationHeader() {
   return (
-    <div>
-      Notification Header
-    </div>
+    <div>Notification Header</div>
   );
 }
 
-function App() {
-  return (
-    <>
-      <Header name="John Deere Training" onButtonClick={(e) => alert('Button Clicked')} />
-      <Notification
-        header={<ExampleNotificationHeader />}
-        notificationStyle={notificationStyles.info}
-      >
-        <p>Notification Custom Content</p>
-        <p>More Custom Content</p>
-      </Notification>
-    </>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showNotification: true
+    };
+
+    this.toggleNotification = this.toggleNotification.bind(this);
+  }
+
+  toggleNotification() {
+    const showNotification = !this.state.showNotification;
+    this.setState({showNotification});
+  }
+
+  render() {
+    return (
+      <>
+        <Header name="John Deere Training" onButtonClick={(e) => alert('Button Clicked')} />
+        <button className="btn-primary" onClick={this.toggleNotification}>
+          {this.state.showNotification ? 'Hide Notification' : 'Show Notification'}
+        </button>
+        {
+          this.state.showNotification &&
+          <Notification
+            header={<ExampleNotificationHeader />}
+            notificationStyle={notificationStyles.info}
+          >
+            <p>Notification Custom Content</p>
+            <p>More Custom Content</p>
+          </Notification>
+        }
+
+      </>
+    );
+  }
 }
 
 export default App;
